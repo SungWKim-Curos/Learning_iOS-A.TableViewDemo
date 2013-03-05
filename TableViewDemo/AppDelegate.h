3d9f24a0bde0b3aaa3559d1ec9ffe80f15857104
sungwkim <sungwkim@curos.co.kr>
2013-03-06 01:59:20 +0900

    Initial Commit

---
+++ TableViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by curos on 3/6/13.
+//  Copyright (c) 2013 curos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TableViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewDemo
+//
+//  Created by curos on 3/6/13.
+//  Copyright (c) 2013 curos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
