47b7d7e9a5cc159c0acdea1afcc232eee2981cfe
sungwkim <sungwkim@curos.co.kr>
2013-03-06 02:08:29 +0900

    The demo button is ready.

--- TableViewDemo/ViewController.m
+++ TableViewDemo/ViewController.m
@@ -1,29 +1,12 @@
-//
-//  ViewController.m
-//  TableViewDemo
-//
-//  Created by curos on 3/6/13.
-//  Copyright (c) 2013 curos. All rights reserved.
-//
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
-- (void)viewDidLoad
+-(IBAction) DemoDidClick
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
