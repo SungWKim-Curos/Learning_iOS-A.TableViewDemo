072195ccbee74553ac623e1153343195fe341316
sungwkim <sungwkim@curos.co.kr>
2013-03-06 11:09:08 +0900

    Call initWithNibName.

--- TableViewDemo/ViewController.m
+++ TableViewDemo/ViewController.m
@@ -1,12 +1,19 @@
 
 #import "ViewController.h"
 
+#import "MyTableViewController.h"
+
 
 @implementation ViewController
 
 -(IBAction) DemoDidClick
 {
+    MyTableViewController* oTblVw =
+        [ [MyTableViewController alloc]
+            initWithNibName:@"MyTableViewController" bundle:nil ] ;
     
+    [ self.navigationController
+        pushViewController:oTblVw animated:TRUE ] ;
 }
 
 @end
