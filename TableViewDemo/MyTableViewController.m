bd72f8c817dca6ee544960147f99b27748bba2d1
sungwkim <sungwkim@curos.co.kr>
2013-03-06 10:49:51 +0900

    Create class MyTableViewController.

---
+++ TableViewDemo/MyTableViewController.m
@@ -0,0 +1,21 @@
+
+#import "MyTableViewController.h"
+
+
+@implementation MyTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if( nil == self ) return nil ;
+
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
